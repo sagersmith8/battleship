6007977b9d6b9676b4ec3aba08e108fa2ee6d7e3
Sam Shissler <samshissler@gmail.com>
2016-09-19 21:21:46 -0600

    Updated style, made fixes

--- battleship/web/server.py
+++ battleship/web/server.py
@@ -1,7 +1,7 @@
 import sys
 from os.path import dirname
 import time
-from bottle import route, run, request, error, static_file
+from bottle import route, run, request, error, static_file, HTTPError
 from jinja2 import Environment, FileSystemLoader
 from battleship.model.armada import Armada
 
@@ -15,8 +15,14 @@ enemy_board = []
 for row in xrange(10):
     enemy_board.append([])
     for col in xrange(10):
-        enemy_board[row].append(0)
+        enemy_board[row].append(-1)
 print enemy_board
+
+board = []
+for row in xrange(10):
+    board.append([])
+    for col in xrange(10):
+        board[row].append(-1)
 
 
 @route('/static/<file>')
@@ -36,9 +42,15 @@ def own_board():
     return respond(
         'own_board.html',
         {
-            'armada': armada
+            'board': board,
+            'background_image': {
+                'C': 'http://www.iboats.com/sites/mastercraft/site_page_19088/images/l_topdownviewofprostarskiboatbymastercraft.png',
+                'B': 'http://www.iboats.com/sites/mastercraft/site_page_19088/images/l_topdownviewofmastercraftsx35boatmodel.png',
+                'S': 'http://stoneycreekcreative.com/bayarea/wp-content/uploads/2013/10/X30_top_profile.png',
+                'D': 'http://www.iboats.com/sites/mastercraft/site_page_19088/images/l_xstartopdownviewshowinglayoutofboat-mastercraft.png',
+                'R': 'http://www.onlyinboards.com/getmetafile/4b337fef-c357-4695-825d-130bdfc16df2/2012-Centurion-Enzo-SV244.aspx'
+            }
         }
-
     )
 
 
@@ -84,8 +96,27 @@ def handle_fire():
     """
     x = request.forms.get('x')
     y = request.forms.get('y')
-    point = (int(x), int(y))
+    if not x or not y:
+        print '404 #1'
+        return HTTPError(404, reason="You used the wrong format, nerd.")
+    if len(y) > 1 or len(x) > 1:
+        print '400'
+        return HTTPError(400, reason="This is not a valid square, nerd.")
+    try:
+        x = int(x)
+        y = int(y)
+    except TypeError:
+        print '404 #2'
+        return HTTPError(404, reason='You used the wrong format, nerd.')
+    point = (x, y)
+    if board[y][x] in [1, 0]:
+        print '410'
+        return HTTPError(410, reason='This square has already been attacked, nerd.')
     hit = armada.check_hit(point)
+    if str(hit) not in 'CBRSD':
+        board[y][x] = hit
+    else:
+        board[y][x] = 1
     print 'Fired at', point, hit
     return (
         'hit={}'.format(hit)
@@ -105,9 +136,11 @@ def update_board():
     """
     x = int(request.forms.get('x'))
     y = int(request.forms.get('y'))
-    shot = int(request.forms.get('shot'))
-    enemy_board[y][x] = shot
-    time.sleep(1)
+    shot = request.forms.get('shot')
+    if str(shot) not in 'CBRSD':
+        enemy_board[y][x] = int(shot)
+    else:
+        enemy_board[y][x] = 2
     return (
         'Updated board'
     )
@@ -124,8 +157,13 @@ def handle_410(*args):
 
 
 @error(400)
-def handle_400():
+def handle_400(*args):
     print '400'
+
+
+@error(500)
+def handle_500(*args):
+    print '500'
 
 
 def print_armada(*args):
@@ -144,5 +182,8 @@ def print_armada(*args):
 if __name__ == '__main__':
     port = sys.argv[1]
     armada = Armada(sys.argv[2])
+    for ship in armada.ships:
+        for location in armada.ships[ship].location:
+            board[location[1]][location[0]] = ship
     print_armada()
     run(host='localhost', port=int(port), debug=True)
